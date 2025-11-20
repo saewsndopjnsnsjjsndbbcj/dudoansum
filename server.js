39ebc5a7c295ad25313b1e104de9595799406705
saewsndopjnsnsjjsndbbcj <hauhhdbnhas@gmail.com>
2025-11-20 15:23:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Khai báo API URL chính xác
-const API_URL = 'https://suncoc.onrender.com/latest'; 
+const API_URL = 'https://lichsusumcl.onrender.com/latest'; 
 
 // =======================
 // BOT AI PREDICTION SYSTEM
